d6b711950947a678d8901c75cab59c998b4b9821
Kaung Htet Aung <kaung.h.ag@gmail.com>
2022-04-28 19:32:22 +0630

    integrate lib apis

--- main.go
+++ main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+
+	code "hack-assembler/lib/code"
+	parser "hack-assembler/lib/parser"
+	symbol "hack-assembler/lib/symbol"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello")
+	fileName := os.Args[1]
+	parser.Parse(fileName)
+
+	outFilename := strings.TrimSuffix(fileName, filepath.Ext(fileName)) + ".hack"
+
+	outFile, err := os.Create(outFilename)
+	check(err)
+	defer outFile.Close()
+
+	writer := bufio.NewWriter(outFile)
+
+	// first pass
+	var i uint16 = 0
+	for parser.HasMoreLines() {
+		parser.Advance()
+		if parser.InstructionType() == parser.L_INSTRUCTION {
+			symbol.AddEntry(parser.Symbol(), i)
+		} else {
+			i++
+		}
+	}
+
+	// second pass
+	var memvar uint16 = 16
+	parser.Parse(fileName)
+	for parser.HasMoreLines() {
+		parser.Advance()
+
+		if parser.InstructionType() == parser.A_INSTRUCTION {
+
+			// check if decimal value
+			if regexp.MustCompile(`^[0-9]+$`).MatchString(parser.Symbol()) {
+				d, _ := strconv.Atoi(parser.Symbol())
+				fmt.Fprintf(writer, "%016b\n", d)
+			} else {
+				if !symbol.Contains(parser.Symbol()) {
+					symbol.AddEntry(parser.Symbol(), memvar)
+					memvar++
+				}
+				fmt.Fprintf(writer, "%016b\n", symbol.GetAddress(parser.Symbol()))
+			}
+		}
+		if parser.InstructionType() == parser.C_INSTRUCTION {
+			fmt.Fprintf(writer, "111%v%v%v\n", code.Comp(parser.Comp()), code.Dest(parser.Dest()), code.Jump(parser.Jump()))
+		}
+	}
+	writer.Flush()
+	fmt.Println("Compilation Finished")
 }
